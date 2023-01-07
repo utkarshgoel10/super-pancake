b188c2181550eaf0fa33e62d28d74f250e0699b4
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2023-01-07 14:31:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { motion } from "framer-motion";
+import React from "react";
+import AnimatedCursor from "react-animated-cursor";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import About from "./components/container/About/About";
+import Contact from "./components/container/Contact/Contact";
+import Footer from "./components/container/Footer/Footer";
+import Portfolio from "./components/container/Portfolio/Portfolio";
+import Skills from "./components/container/Skills/Skills";
+import SmoothScroll from "./components/container/SmoothScroll/SmoothScroll";
+import Navbar from "./components/Navbar/Navbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <motion.div>
+      <AnimatedCursor
+        color="255,255,255"
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={1.7}
+        outerAlpha={0}
+        outerStyle={{
+          border: "3px solid #fff",
+        }}
+      />
+      <SmoothScroll>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/home" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+      </SmoothScroll>
+      </motion.div>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
