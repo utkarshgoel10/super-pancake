e21de10d0ef71a54bda089e28ee2a3dac8e5daa2
Utkarsh Goel <66838142+utkarshgoel10@users.noreply.github.com>
2023-01-07 14:32:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from 'react'
-import "./App.css"
-import About from './components/container/About/About'
-import Contact from './components/container/Contact/Contact'
-import Footer from './components/container/Footer/Footer'
-import Home from './components/container/Home/Home'
-import Portfolio from './components/container/Portfolio/Portfolio'
-import Skills from './components/container/Skills/Skills'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import "./App.css";
+import About from "./components/container/About/About";
+import Contact from "./components/container/Contact/Contact";
+import Home from "./components/container/Home/Home";
+import Portfolio from "./components/container/Portfolio/Portfolio";
+import Skills from "./components/container/Skills/Skills";
 import Marquee from "react-fast-marquee";
-import { motion } from 'framer-motion';
-import SmoothScroll from './components/container/SmoothScroll/SmoothScroll'
-import AnimatedCursor from "react-animated-cursor"
+import { motion } from "framer-motion";
 const App = () => {
   return (
     <motion.div>
-    <AnimatedCursor
+      {/* <AnimatedCursor
         color="255,255,255"
         innerSize={8}
         outerSize={35}
@@ -22,40 +18,53 @@ const App = () => {
         outerScale={1.7}
         outerAlpha={0}
         outerStyle={{
-          border: '3px solid #fff',
+          border: "3px solid #fff",
         }}
-        />
-    <SmoothScroll>        
-      <Navbar />
-      <Home />
-      <About />
-      <Marquee className="mq-t" speed={130} gradient={false}>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>React</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Express</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>NodeJs</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>MongoDB</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>NextJs</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>PHP</h1>  
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Python</h1>   
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Cloud</h1>         
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Linux</h1>
-          
+      /> */}
+      {/* <SmoothScroll> */}
+        {/* <Navbar /> */}
+        {/* <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/home" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer /> */}
+        <Home />
+        <About />
+        <Marquee className="mq-t" speed={130} gradient={false}>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>React</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Express</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>NodeJs</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>MongoDB</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>NextJs</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>PHP</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Python</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Cloud</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Linux</h1>
         </Marquee>
-        <Marquee className="mq-b" speed={130} gradient={false} direction="right">
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Software Engineer</h1>    
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Freelancing</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Blockchain</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Video Editing</h1>
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Instructor</h1>                          
-          <h1 style={{padding: '2rem', color: '#FFF'}}>Open Source</h1>
+        <Marquee
+          className="mq-b"
+          speed={130}
+          gradient={false}
+          direction="right"
+        >
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Software Engineer</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Freelancing</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Blockchain</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Video Editing</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Instructor</h1>
+          <h1 style={{ padding: "2rem", color: "#FFF" }}>Open Source</h1>
         </Marquee>
-      <Skills />
-      <Portfolio />      
-      <Contact />
-      <Footer />
-      </SmoothScroll>
+        <Skills />
+        <Portfolio />
+        <Contact />
+        {/* <Footer /> */}
+      {/* </SmoothScroll> */}
     </motion.div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
